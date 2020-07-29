36c024727714e51a27814586cb59da96eff0ec5e
oranged <1208917130@qq.com>
2020-07-29 17:31:04 +0800

    新增了后台管理系统

---
+++ after/src/settings.js
@@ -0,0 +1,35 @@
+module.exports = {
+  title: 'Vue Element Admin',
+
+  /**
+   * @type {boolean} true | false
+   * @description 是否显示右边的设置
+   */
+  showSettings: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description 是否显示打开的页面
+   */
+  tagsView: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description 固定头部
+   */
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description 显示侧边导航栏图标
+   */
+  sidebarLogo: true,
+
+  /**
+   * @type {string | array} 'production' | ['production', 'development']
+   * @description Need show err logs component.
+   * The default is only used in the production env
+   * If you want to also use it in dev, you can pass ['production', 'development']
+   */
+  errorLog: 'production'
+}
